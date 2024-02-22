d8de7b7c82ff76d63e5495da5e19a3feba5ab12b
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-02-22 23:35:13 +0200

    markup completed

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,18 +1,16 @@
 import $ from "jquery";
-// const closeOpenText = document.querySelectorAll(".changeStatus");
 
-// $(".question").click(function () {
-//   $(this).find(".content").toggle(200); //скрытие, показ ответа
-//   $(this).find(".question-plus").css("transform", "rotate(0)"); //поворот стрелки
-//   if ($(this).hasClass("open")) {
-//     $(this).removeClass("open");
-//     closeOpenText.forEach((item) => (item.textContent = "Відкрити опис"));
-//   } else {
-//     $(this).addClass("open");
-//     closeOpenText.forEach((item) => (item.textContent = "Закрити опис"));
-//     $(this).find(".question-plus").css("transform", "rotate(45deg)"); //поворот стрелки
-//   }
-// });
+$(".question").click(function () {
+  $(this).find(".content").toggle(200); //скрытие, показ ответа
+  $(this).find(".question-plus").css("transform", "rotate(0)"); //поворот стрелки
+  if ($(this).hasClass("open")) {
+    $(this).removeClass("open");
+    $(this).find(".changeStatus").text("Відкрити опис");
+  } else {
+    $(this).addClass("open");
+    $(this).find(".changeStatus").text("Закрити опис");
+  }
+});
 
 $(".question-two").click(function () {
   $(this).find(".content-two").toggle(200); //скрытие, показ ответа
@@ -24,3 +22,13 @@ $(".question-two").click(function () {
     $(this).find(".question-plus").css("transform", "rotate(45deg)"); //поворот стрелки
   }
 });
+
+//   if ($(this).hasClass("open")) {
+//     $(this).removeClass("open");
+//     closeOpenText.forEach((item) => (item.textContent = "Відкрити опис"));
+//   } else {
+//     $(this).addClass("open");
+//     closeOpenText.forEach((item) => (item.textContent = "Закрити опис"));
+//     $(this).find(".question-plus").css("transform", "rotate(45deg)"); //поворот стрелки
+//   }
+// });
